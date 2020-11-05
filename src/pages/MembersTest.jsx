04fe194b9f8f734badb88cd8f61a3ab99ebd9c44
Ctorum <vhcabralrezende@hotmail.com>
2020-11-05 19:01:23 -0300

    Vito added

--- src/pages/MembersTest.jsx
+++ src/pages/MembersTest.jsx
@@ -1,85 +1,13 @@
 import React, { useState } from 'react';
 
-import { Container, MembersDiv } from '../styles/pages/membersTest';
-import NavBar from '../components/pages/NavBar';
-
-import igoImage from '../assets/members/Igo.png';
-import isaImage from '../assets/members/Isa.jpeg';
-import lauraImage from '../assets/members/Laura.jpeg';
-import vidroImage from '../assets/members/Vidro.jpeg';
+import { Container } from '../styles/pages/membersTest';
 
 function MembersTest() {
-  const [text, setText] = useState('');
-  const [hovering, setHovering] = useState(false);
-
-  const changeValues = (textState) => {
-    setText(textState);
-    setHovering(true);
-  };
-
-  const animationTimeOut = () => {
-    setTimeout(() => {
-      setHovering(false);
-    }, 1000);
-  };
-
-  if (!hovering) {
-    animationTimeOut();
-  }
-
   return (
     <Container>
-      <NavBar />
-      <main>
-        <h1>Integrantes</h1>
-        <p>Aqueles que deram seu tempo para que consiga ver seus astros</p>
-        <MembersDiv hovering={hovering}>
-          <div
-            onMouseEnter={() => {
-              changeValues(
-                'Desenvolvedor full stack , além de matemático e físico. Adora resolver enigmas. Desbravador há 2 anos. 16 anos de pura sedução.'
-              );
-            }}
-            className="member"
-          >
-            <img src={igoImage} alt="igo" />
-          </div>
-          <div
-            onMouseEnter={() => {
-              changeValues(
-                'Domina tanto humanas quanto exatas. Ama astronomia e filosofia. Seu passatempo preferido é ler e maratonar séries. Aspirante a escritora. 16 anos de puro existencialismo.'
-              );
-            }}
-            className="member"
-          >
-            <img src={isaImage} alt="isa" />
-          </div>
-        </MembersDiv>
-        <div className="span">{text}</div>
-        <MembersDiv hovering={hovering}>
-          <div
-            onMouseEnter={() => {
-              changeValues(
-                'Trabalhou na Diretoria Regional de Educação por 1 ano. Desbravadora há 6 anos. Adora maquiagem, esportes e história. 16 anos de puro carinho.'
-              );
-            }}
-            className="member"
-          >
-            <img src={lauraImage} alt="laura" />
-          </div>
-          <div
-            onMouseEnter={() => {
-              changeValues(
-                'Desenvolvedor full stack , além de matemático e químico nas horas vagas. Desbravador há 3 anos. 16 anos de pura de sedução.'
-              );
-            }}
-            className="member"
-          >
-            <img src={vidroImage} alt="vidro" />
-          </div>
-        </MembersDiv>
-      </main>
-      <footer />
+      <section className="starter">
+        <button type="button">Start</button>
+      </section>
     </Container>
   );
 }
--- src/styles/pages/membersTest.jsx
+++ src/styles/pages/membersTest.jsx
@@ -1,88 +1,40 @@
 import styled from 'styled-components';
 
+const getRandom = (value) => {
+  return Math.floor(Math.random() * value) + 1;
+};
+
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-image: url('https://wallpapershome.com/images/pages/pic_h/18295.jpg');
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  h1,
-  h2,
-  p,
-  span {
-    font-weight: 300;
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    color: white;
-  }
-  nav {
-    width: 100%;
-    height: 50px;
-    padding: 0 30px;
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  font-family: 'Poppins', sans-serif;
+  background-color: black;
+  color: white;
+  .starter {
+    width: 100vw;
+    height: 100vh;
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #00000030;
-    div {
-      width: 20%;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: space-between;
-      .link {
-        text-decoration: none;
-        color: white;
-      }
-    }
-  }
-  main {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex: 1;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    h1 {
-      font-size: 48px;
-    }
-    p {
-      font-size: 16px;
-      padding: 0 3px;
-    }
-    .span {
-      width: 450px;
-      height: 300px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      position: absolute;
+    button {
+      background-color: transparent;
+      border: none;
+      font-family: 'Poppins', sans-serif;
       color: white;
+      font-size: 50px;
+      cursor: pointer;
+      &:focus {
+        box-shadow: 0 0 0 0;
+        border: 0 none;
+        outline: 0;
+      }
     }
   }
 `;
 
-export const MembersDiv = styled.div`
-  width: 25%;
-  height: 67%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  transition: 300ms ease-in-out;
-
-  img {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-    object-fit: cover;
-
-    &:hover {
-    }
-  }
+export const Stars = styled.div`
+  width: ${getRandom(window.innerWidth)};
+  height: ${getRandom(window.innerHeight)};
 `;
