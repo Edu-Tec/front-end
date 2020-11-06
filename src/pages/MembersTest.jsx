e954868748cb103856d828d14ccebd17035f3a8b
Ctorum <vhcabralrezende@hotmail.com>
2020-11-06 15:11:22 -0300

    Stars

--- src/pages/MembersTest.jsx
+++ src/pages/MembersTest.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from '../styles/pages/membersTest';
+import { Container, Stars } from '../styles/pages/membersTest';
 
 function MembersTest() {
+  const [starsNumber, setStarsNumber] = useState([]);
+
+  const getRandom = (value, initialValue) => {
+    return Math.floor(Math.random() * value) + initialValue;
+  };
+
+  useEffect(() => {
+    for (let i = 0; i < 200; i += 1) {
+      setStarsNumber((starsNumber) => [...starsNumber, i]);
+    }
+  }, []);
   return (
     <Container>
+      {starsNumber.map((item) => (
+        <Stars
+          left={`${getRandom(window.innerWidth, 1)}px`}
+          top={`${getRandom(window.innerHeight, 1)}px`}
+          size={`${getRandom(5, 1)}px`}
+          shineTime={`${getRandom(10, 2)}s`}
+          key={item}
+        />
+      ))}
       <section className="starter">
         <button type="button">Start</button>
       </section>
--- src/styles/pages/membersTest.jsx
+++ src/styles/pages/membersTest.jsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { AiOutlineStar } from 'react-icons/ai';
 
 const getRandom = (value) => {
   return Math.floor(Math.random() * value) + 1;
@@ -11,7 +12,11 @@ export const Container = styled.div`
   font-family: 'Poppins', sans-serif;
   background-color: black;
   color: white;
+  overflow-x: hidden;
+  overflow-y: hidden;
   .starter {
+    overflow-x: hidden;
+    overflow-y: hidden;
     width: 100vw;
     height: 100vh;
     display: flex;
@@ -34,7 +39,30 @@ export const Container = styled.div`
   }
 `;
 
+const shineAnimation = keyframes`
+  0%, 100%{
+    -webkit-box-shadow: 0px 0px 50px 6.25px rgba(255, 255, 255, 0.75);
+  -moz-box-shadow: 0px 0px 50px 6.25px rgba(255, 255, 255, 0.75);
+  box-shadow: 0px 0px 50px 6.25px rgba(255, 255, 255, 0.75);
+  }
+  50%{
+    -webkit-box-shadow: 0px 0px 100px 6.25px rgba(255, 255, 255, 0.75);
+  -moz-box-shadow: 0px 0px 100px 6.25px rgba(255, 255, 255, 0.75);
+  box-shadow: 0px 0px 100px 6.25px rgba(255, 255, 255, 0.75);
+  }
+`;
+
 export const Stars = styled.div`
-  width: ${getRandom(window.innerWidth)};
-  height: ${getRandom(window.innerHeight)};
+  will-change: box-shadow;
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  position: absolute;
+  background-color: white;
+  border-radius: 50%;
+  left: ${(props) => props.left};
+  top: ${(props) => props.top};
+  /* filter: blur(0.45px); */
+  overflow: hidden;
+  animation: ${shineAnimation} ${(props) => props.shineTime} ease-in-out
+    infinite;
 `;
